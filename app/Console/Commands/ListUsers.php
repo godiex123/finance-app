54e1fb68d4ecbaacc30d49efea6709811a2adeb6
Diego Santiago <diegoa.santiagoh@gmail.com>
2025-05-31 23:44:00 -0400

    por si se pierden el usuario

---
+++ app/Console/Commands/ListUsers.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class ListUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:list-users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::select('email');
+        $this->info('Usuarios:');
+        foreach ($users as $email) {
+            $this->line($email);
+        }
+    }
+}
